24a8b82a87c85a23ef811455b4d1b96231e0cd85
Philip Louie Templonuevo <philiplouietemplonuevo@yahoo.ca>
2018-03-19 20:41:02 +0000

    Set initial state and passed currentUser using props.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,30 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state =  {
+      currentUser: { name: "Philip" },
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
         <NavBar />
         <MessageList />
-        <ChatBar />
+        <ChatBar user={this.state.currentUser} />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+
+    const name = this.props.user.name;
+
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" defaultValue={name} disabled />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
